ab1e2e94a5964bfd5f2303cd1cc432f6e312c557
Eduardo Da Veiga Spiandorello <eduardo.spiandorello@gmail.com>
2021-03-22 23:51:42 -0300

    Add documentation with swagger t0 categories route

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,13 @@
 import express from 'express';
+import swaggerUi from 'swagger-ui-express';
+
+import swaggerDocument from '../swagger.json';
+
 import router from './routes';
 
 const app = express();
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use(express.json());
 
