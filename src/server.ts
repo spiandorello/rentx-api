15c223a26df3ae21b4a5bb038c4cbe9f1dc4adda
Eduardo Da Veiga Spiandorello <eduardo.spiandorello@gmail.com>
2021-03-27 20:45:06 -0300

    feat: add postgress at docker-compose

--- src/server.ts
+++ src/server.ts
@@ -5,6 +5,8 @@ import swaggerDocument from '../swagger.json';
 
 import router from './routes';
 
+const DEFAULT_PORT = 3000;
+
 const app = express();
 
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
@@ -13,4 +15,6 @@ app.use(express.json());
 
 app.use(router);
 
-app.listen(3000);
+app.listen(DEFAULT_PORT, () =>
+    console.log(`Listen port ${DEFAULT_PORT}`)
+);
